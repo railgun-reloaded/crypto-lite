9c77bea8a5ac1fc7606a1cfede9b2328a88c71f5
zy0n.bear <zy0n@users.noreply.github.com>
2025-09-07 18:44:11 -0400

    feat: implement Eddsa class with Poseidon signing and verification methods

---
+++ src/eddsa.ts
@@ -0,0 +1,101 @@
+/* eslint-disable jsdoc/require-jsdoc */
+// @ts-ignore: type-error
+import createBlakeHash from 'blake-hash'
+// @ts-ignore: type-error
+import { Scalar } from 'ffjavascript'
+
+import buildBabyJub from './babyjub.js'
+import buildPoseidon from './poseidon_opt.js'
+
+class Eddsa {
+  babyJub: any
+  poseidon: any
+  F: any
+  constructor (babyJub: any, poseidon: any) {
+    this.babyJub = babyJub
+    this.poseidon = poseidon
+    this.F = babyJub.F
+  }
+
+  pruneBuffer (buff: Uint8Array) {
+    buff[0] = buff[0]! & 0xf8
+    buff[31] = buff[31]! & 0x7f
+    buff[31] = buff[31] | 0x40
+    return buff
+  }
+
+  prv2pub (prv: Uint8Array) {
+    // uneeded
+    // const F = this.babyJub.F;
+    const sBuff = this.pruneBuffer(
+      createBlakeHash('blake512').update(Buffer.from(prv)).digest()
+    )
+    const s = Scalar.fromRprLE(sBuff, 0, 32)
+    const A = this.babyJub.mulPointEscalar(
+      this.babyJub.Base8,
+      Scalar.shr(s, 3)
+    )
+    return A
+  }
+
+  signPoseidon (prv: Uint8Array, msg: Uint8Array) {
+    const F = this.babyJub.F
+    const sBuff = this.pruneBuffer(
+      createBlakeHash('blake512').update(Buffer.from(prv)).digest()
+    )
+    const s = Scalar.fromRprLE(sBuff, 0, 32)
+    const A = this.babyJub.mulPointEscalar(
+      this.babyJub.Base8,
+      Scalar.shr(s, 3)
+    )
+
+    const composeBuff = new Uint8Array(32 + msg.length)
+    composeBuff.set(sBuff.slice(32), 0)
+    F.toRprLE(composeBuff, 32, msg)
+    const rBuff = createBlakeHash('blake512')
+      .update(Buffer.from(composeBuff))
+      .digest()
+    const r = Scalar.mod(Scalar.fromRprLE(rBuff, 0, 64), this.babyJub.subOrder)
+    const R8 = this.babyJub.mulPointEscalar(this.babyJub.Base8, r)
+
+    const hm = this.poseidon([R8[0], R8[1], A[0], A[1], msg])
+    const hms = Scalar.e(this.babyJub.F.toObject(hm))
+
+    const S = Scalar.mod(
+      Scalar.add(r, Scalar.mul(hms, s)),
+      this.babyJub.subOrder
+    )
+    return {
+      R8,
+      S,
+    }
+  }
+
+  verifyPoseidon (msg: Uint8Array, sig: any, A: any) {
+    if (typeof sig !== 'object') return false
+    if (!Array.isArray(sig.R8)) return false
+    if (sig.R8.length !== 2) return false
+    if (!this.babyJub.inCurve(sig.R8)) return false
+    if (!Array.isArray(A)) return false
+    if (A.length !== 2) return false
+    if (!this.babyJub.inCurve(A)) return false
+    if (sig.S >= this.babyJub.subOrder) return false
+
+    const hm = this.poseidon([sig.R8[0], sig.R8[1], A[0], A[1], msg])
+    const hms = Scalar.e(this.babyJub.F.toObject(hm))
+
+    const Pleft = this.babyJub.mulPointEscalar(this.babyJub.Base8, sig.S)
+    let Pright = this.babyJub.mulPointEscalar(A, Scalar.mul(hms, 8))
+    Pright = this.babyJub.addPoint(sig.R8, Pright)
+
+    if (!this.babyJub.F.eq(Pleft[0], Pright[0])) return false
+    if (!this.babyJub.F.eq(Pleft[1], Pright[1])) return false
+    return true
+  }
+}
+
+export default function buildEddsa () {
+  const babyJub = buildBabyJub()
+  const poseidon = buildPoseidon()
+  return new Eddsa(babyJub, poseidon)
+}
