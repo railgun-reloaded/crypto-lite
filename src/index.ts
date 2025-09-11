c57fdf568ad881870d15484b21a672e09f27f662
zy0n.bear <zy0n@users.noreply.github.com>
2025-09-11 12:58:21 -0400

    refactor: remove unused imports and commented code in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,12 @@
 /* eslint-disable jsdoc/require-jsdoc */
 
 // @ts-ignore TODO: typefix
-// export { randomBytes } from '@noble/curves/utils.js';
 import {
-  //  ExtendedPoint as Point,
   getPublicKey
 } from '@noble/ed25519'
-// import * as ed from '@noble/ed25519'
-// import { blake512 } from '@noble/hashes/blake1'
-// console.log(blake512)
 
 import buildEddsa from './eddsa-noble'
-// @ts-ignore TODO: typefix
 import { bigIntToUint8Array, uint8ArrayToBigInt } from './math'
-
-// import { ShieldNoteERC20 } from './notes/shield-note-erc20.js';
-
-// export * from './notes'
-
-// maybe this is no longer needed?
-// ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m))
-
-// const poseidonPromise = buildPoseidonOpt();
 
 interface CircomlibSignature {
   R8: [Uint8Array, Uint8Array];
@@ -38,27 +23,9 @@ export const wrapperModule = {
   eddsaBuild,
   poseidonBuild
 }
-// initialize sha512 sync
 
 export const poseidon = (inputs: Uint8Array[]) => {
-  // prefer wasm, (dev) must be manually initialized as such.
-  // eddsaBuild = eddsaPromise;
-  // console.log(eddsaBuild.babyjubjub)
-  // const _poseidon = poseidonBuild; // typeof typeof poseidonBuild.wasm === 'undefined' ? poseidonBuild.pure : poseidonBuild.wasm
-  // if (typeof _poseidon === 'undefined') {
-  //   throw new Error('Poseidon has not been loaded.');
-  // }
-  // // poseidon expect input of bigint
-  // const _inputs = inputs.map((input) =>
-  //   _poseidon.F.toMontgomery(new Uint8Array(input).reverse()),
-  // );
-  // const result =
-  //   _poseidon.F.fromMontgomery(
-  //     _poseidon(
-  //       _inputs,
-  //     ),
-  //   );
-  // console.log('result', result)
+  // TODO: wasm import
   const result = poseidonBuild(inputs)
   return bigIntToUint8Array(result).reverse()
 }
@@ -77,23 +44,12 @@ export const signPoseidon = (
   if (typeof eddsaBuild === 'undefined') {
     throw new Error('Invalid')
   }
-  // Get montgomery representation
   const montgomery = eddsaBuild.toMontgomery(
     new Uint8Array(message).reverse()
   )
-  // Sign
   const sig = eddsaBuild.signPoseidon(key, montgomery)
-  // console.log('SIGNED', sig)
-  // Convert R8 elements from montgomery and to BE
   const r8 = sig.R8
-  // .map((element: any) =>
-  //   eddsaBuild.F.fromMontgomery(element).reverse()
-  // )
-  // 32 byte inputs only right now
   return [bigIntToUint8Array(r8.x).reverse(), bigIntToUint8Array(r8.y).reverse(), bigIntToUint8Array(sig.S as any as bigint)]
-  // reversing matches output from railgun-reloaded/cryptography
-  // still ned to confirm thatis valid. does not currently pass /verify/ function
-  // return [r8[0], r8[1], bigIntToUint8Array(sig.S).reverse()]
 }
 
 export const verifyEDDSA = (message: Uint8Array, signature: CircomlibSignature, pubkey: [Uint8Array, Uint8Array]) => {
@@ -103,8 +59,6 @@ export const verifyEDDSA = (message: Uint8Array, signature: CircomlibSignature, 
   const montgomery = eddsaBuild.fromMontgomery(
     new Uint8Array(message).reverse()
   )
-  // console.log('SIGGGGGG', signature)
-  //  use raw values
   const r8 = signature.R8.map((element: any) => uint8ArrayToBigInt(element.reverse()))
   const newSig = {
     R8: { x: r8[0]!, y: r8[1]! },
@@ -115,7 +69,6 @@ export const verifyEDDSA = (message: Uint8Array, signature: CircomlibSignature, 
   return eddsaBuild.verifyPoseidon(montgomery, newSig, { x: newPubKey[0]!, y: newPubKey[1]! })
 }
 
-// used for getPublicSpendingKey
 export const privateKeyToPublicKey = (
   privateKey: Uint8Array
 ): [Uint8Array, Uint8Array] | any => {
@@ -126,7 +79,6 @@ export const privateKeyToPublicKey = (
 }
 
 export const getPublicSpendingKey = (privateKey: Uint8Array): [Uint8Array, Uint8Array] => {
-  // convert this from
   if (privateKey.length !== 32) throw Error('Invalid private key length')
   return privateKeyToPublicKey(privateKey)
 }
@@ -136,6 +88,3 @@ export const getPublicViewingKey = (
 ): Uint8Array => {
   return getPublicKey(privateViewingKey)
 }
-
-// add in aes operations
-// recheck bn254
